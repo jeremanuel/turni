4783487a922c2f75f5e304a36f387af5b845aab6
AgustinIMassa <156153208+AgustinIMassa@users.noreply.github.com>
2024-04-14 15:42:17 -0300

    Cross platform local storage & Cross Endpoint BE (#10)
    * update de packages por merge
    
    * Feature/page login (#6)
    
    * svg images
    
    * avances con la pantalla de login, boton mobile
    
    * google button
    
    * cambio un asset a su version en blanco
    
    * fix library conflicts in native and ignore compile or build files
    
    * delete ios folder and add to gitignore
    
    * delete android folter
    
    * web folder delete
    
    * ignore vscode things
    
    * elimino .vscode
    
    * android config
    
    * login funcionando en web y nativo
    
    * remove print
    
    * se desabilita el backup automatico que hace android en google drive por defecto en el manifesto, para que manejemos eso nosotros desde el package secure storage
    
    * se termina de configurar android, se habilita la extension de flutter secure storage con una rule en el manifest
    
    * se agregan cositas para el storage de android, y se agrega una nueva env, para el emulador del android para que funcione la pegada al be
    
    ---------
    
    Co-authored-by: Joaquin Mastropierro <joaquinmastropierro@gmail.com>
    Co-authored-by: jerex3 <jmanuel@sinergiasoluciones.com.ar>

--- lib/presentation/auth/login_page.dart
+++ lib/presentation/auth/login_page.dart
@@ -1,65 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:turni/core/config/service_locator.dart';
-import 'package:turni/presentation/core/cubit/auth/auth_cubit.dart';
-import 'package:turni/presentation/core/input/custom_outlined_button.dart';
-import 'package:turni/presentation/auth/widgets/web/google_button_web.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../core/config/service_locator.dart';
+import '../core/cubit/auth/auth_cubit.dart';
+import 'widgets/google_button.dart';
 
 class LoginPage extends StatelessWidget {
-
-  LoginPage({super.key});
-
-  final authCubit = sl<AuthCubit>();
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset("assets/img/logo1.png"),
-          const SizedBox(
-            height: 50,
+      body: BlocListener<AuthCubit, AuthState>(
+        bloc: sl<AuthCubit>(),
+        listener: (context, state) => {
+          if (state is AuthError)
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(state.error ?? "Test"),
+              duration: const Duration(days: 1),
+            ))
+        },
+        child: Container(
+          padding: const EdgeInsets.all(50),
+          decoration: backgroundColor,
+          child: Center(
+            child: Column(
+              children: [
+                buildHeader(),
+                const SizedBox(height: 20),
+                buildBackgroundImage(),
+                const Spacer(),
+                buildGoogleButton()
+              ],
+            ),
           ),
-          const Center(
-            child: Text("Bienvenido", style: TextStyle(fontSize: 25))
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          buildGoogleButton(context)
-        ],
+        ),
       ),
     );
   }
 
-  Widget buildGoogleButton(context) {
+  final BoxDecoration backgroundColor = const BoxDecoration(
+    gradient: LinearGradient(
+      colors: [
+        Color.fromRGBO(189, 163, 246, 1),
+        Color.fromRGBO(103, 43, 234, 1)
+      ],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+    ),
+  );
 
+  Widget buildHeader() {
+    const String logotype = 'assets/img/logotype_white.svg';
 
-    if(kIsWeb){
-      return const GoogleButtonWeb();
-    }
-
-    return CustomOutlinedButton(
-      child: Row(
-            
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 30,
-                width: 30,
-                child: Image.network('http://pngimg.com/uploads/google/google_PNG19635.png', fit: BoxFit.cover),
-              ),
-              const SizedBox(width: 15),
-              const Text("Entra con Google", style: TextStyle(color: Colors.black87)),                          
-            ],
-          ),
-      onPressed:() async {
-            await authCubit.signInGoogle();
-          },
+    return SvgPicture.asset(
+      logotype,
+      semanticsLabel: 'Logo de la app de Turni',
+      width: 138,
+      height: 46,
     );
+  }
 
+  Widget buildBackgroundImage() {
+    const String backgroundIsotype = 'assets/img/isotype_white.svg';
+
+    return SizedBox(
+        height: 100,
+        width: 100,
+        child: OverflowBox(
+          maxWidth: 600,
+          maxHeight: 600,
+          alignment: Alignment.topCenter,
+          child: ColorFiltered(
+              colorFilter: const ColorFilter.mode(
+                Color.fromRGBO(
+                    205, 185, 248, 0.18), // Establece la opacidad aquí
+                BlendMode.srcIn,
+              ),
+              child: SvgPicture.asset(
+                backgroundIsotype,
+                excludeFromSemantics: true,
+                height: 1200,
+                width: 1200,
+              )),
+        ));
+  }
+
+  Widget buildGoogleButton() {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 70),
+      child: const GoogleButton(),
+    );
   }
 }
-
-
