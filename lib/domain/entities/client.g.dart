6440ee50620dd99bc7e1e6e432e7eeb32c0fc872
Agustin Massa <agus.massaing@gmail.com>
2024-04-14 15:12:22 -0300

    se agregan cositas para el storage de android, y se agrega una nueva env, para el emulador del android para que funcione la pegada al be

--- lib/domain/entities/client.g.dart
+++ lib/domain/entities/client.g.dart
@@ -10,10 +10,14 @@ Client _$ClientFromJson(Map<String, dynamic> json) => Client(
       clientId: ValueTransformers.fromJsonString(json['client_id']),
       personId: ValueTransformers.fromJsonString(json['person_id']),
       userId: ValueTransformers.fromJsonString(json['user_id']),
+      person: json['person'] == null
+          ? null
+          : Person.fromJson(json['person'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$ClientToJson(Client instance) => <String, dynamic>{
       'client_id': instance.clientId,
       'person_id': instance.personId,
       'user_id': instance.userId,
+      'person': instance.person,
     };
