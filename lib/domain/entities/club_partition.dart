a7cd7683fcbf708c46c278c45ef05b2c74a2636a
jerex3 <jmanuel@sinergiasoluciones.com.ar>
2024-05-30 12:47:18 -0300

    Agregue fromJsons implementados previamente

--- lib/domain/entities/club_partition.dart
+++ lib/domain/entities/club_partition.dart
@@ -1,10 +1,12 @@
 
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../../core/utils/value_transformers.dart';
 import 'club_type.dart';
 import 'physical_partition.dart';
 
 part 'club_partition.freezed.dart';
+part 'club_partition.g.dart';
 
 @freezed
 class ClubPartition with _$ClubPartition {
@@ -13,13 +15,20 @@ class ClubPartition with _$ClubPartition {
     const factory ClubPartition({
       int? club_partition_id,
       required int club_id,
+      @JsonKey(fromJson: ValueTransformers.fromJsonInt)
       required int club_type_id,
-      required String phone,
+      String? phone,
 
       // Relations
+      @JsonKey(name: "partition_physical")
       List<PhysicalPartition>? physicalPartitions,
+       @JsonKey(name: "club_type")
       ClubType? clubType
 
   }) = _ClubPartition;
+
+
+    factory ClubPartition.fromJson(Map<String, dynamic> json) => _$ClubPartitionFromJson(json);
+
 
 }
