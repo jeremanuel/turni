223d8dd1e2e5923970a4e7cc0919b9c8098ce6eb
jerex3 <jmanuel@sinergiasoluciones.com.ar>
2024-10-06 21:45:12 -0300

    Clase con utilidades de fechas

---
+++ lib/core/utils/date_functions.dart
@@ -0,0 +1,29 @@
+import 'package:intl/intl.dart';
+
+class DateFunctions {
+
+  /// @Param todayAndTomorrow 
+  /// Indica si tiene que usar textos 'hoy', 'mañana', 'ayer' para representar esos dias. 
+  static String formatDateToDayMonth(DateTime date, [bool todayAndTomorrow = true]){
+    
+    if(!todayAndTomorrow) return DateFormat.MMMd().format(date);
+
+    final aDate = DateTime(date.year, date.month, date.day);
+
+    final now = DateTime.now().copyWith(hour: 0,second: 0, minute: 0, microsecond: 0, millisecond: 0);
+
+    if(aDate == now) return "Hoy";
+
+    final tomorrow = DateTime(now.year, now.month, now.day + 1);
+
+    if(aDate == tomorrow) return "Mañana";
+
+    final yesterday = DateTime(now.year, now.month, now.day - 1);
+
+
+    if(aDate == yesterday) return "Ayer";
+
+    return DateFormat.MMMd().format(date);
+
+  }
+}
